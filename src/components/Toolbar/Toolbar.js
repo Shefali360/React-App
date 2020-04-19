bbbb355dc9c3bc49e32a08d2a5b7d3892e9bc7fe
Shefali360 <shefali.goyal@tothenew.com>
2020-04-19 22:43:55 +0530

    Slight changes to toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -2,15 +2,18 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../Logo/Logo';
 import Navbar from '../Navbar/NavbarItems';
+import {Link} from 'react-router-dom';
 
 const Toolbar=(props)=>{
 return(
 
 <div className={styles.Toolbar}>
 <div className={styles.Logo}>
-<Logo/>
+<Link to="/home"className={styles.Link}><Logo/>
 <span className={styles.Sitetag}>COVID'19</span>
+</Link>
 </div>
+
 <Navbar/>
 
 </div>
